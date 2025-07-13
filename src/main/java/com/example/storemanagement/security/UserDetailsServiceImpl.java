e0aba0a22a7fb9d6c31e8ed65db517897b1495a7
Mihai <popescu.mihai@vego.holdings>
2025-07-13 21:41:43 +0300

    Correction: use constructor injection insetad using @Autowired

--- src/main/java/com/example/storemanagement/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/storemanagement/security/UserDetailsServiceImpl.java
@@ -3,14 +3,16 @@ package com.example.storemanagement.security;
 import com.example.storemanagement.model.AppUser;
 import com.example.storemanagement.repository.AppUserRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-	@Autowired
-    private AppUserRepository repo;
+	private AppUserRepository repo;
+    
+	public UserDetailsServiceImpl(AppUserRepository productRepository) {
+        this.repo = productRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
